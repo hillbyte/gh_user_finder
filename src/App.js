02b41b1795a74b95631cbd49420086fc87db96bb
hillbyte <ahillbaky@gmail.com>
2021-07-29 17:58:21 +0530

    env updated

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
   };
 
   onTermSubmit = async (term) => {
-    let client_id =process.env.REACT_APP_CLIENT_ID;
-    let client_secret =process.env.REACT_APP_CLIENT_SECRET;
+    let client_id ="Iv1.83acb85739487d9e";
+    let client_secret ="afefad274249171ec50ebdb1551394b72c676433";
     let response = await axios.get(
       `https:api.github.com/users/${term}?Client_Id${client_id}&Client_Secret${client_secret}`
     );
